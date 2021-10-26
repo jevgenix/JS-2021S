572f5e2e8a5ba01f0a43f4cb22d8e68783ef0a27
jevgenix <71660541+jevgenix@users.noreply.github.com>
2021-10-26 19:57:10 +0300

    todo-list

---
+++ dom-project/index.js
@@ -0,0 +1,15 @@
+/*    function addToDo() {
+                var text = document.getElementById("lista").value;
+                var node = document.createElement("li");
+                var textnode = document.createTextNode(text);
+                node.appendChild(textnode);
+                node.className = 'item';
+                document.getElementById("myList").appendChild(node);
+                node.appendChild(btn);
+                
+                
+                tallena();
+    
+    
+                palautaKenttä();
+            } */
